f2b127e80d7b1aa63bcc10f06e7cd3bbdae3b29d
zulqarnainali96 <zuhairbaig23@gmail.com>
2023-10-02 17:16:13 +0500

    add origin frontend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'https://mi-banana-3fxy3bxtx-zulqarnainali96.vercel.app/',
     'http://localhost:4005',
     'http://localhost:3001',
     'http://localhost:5173',
