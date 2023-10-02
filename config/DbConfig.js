33f9f2c0df7f94c01c9f2fba2b49b7b175c6b9e4
zulqarnainali96 <zuhairbaig23@gmail.com>
2023-10-02 22:17:59 +0500

    changes in packg.json

--- config/DbConfig.js
+++ config/DbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const ConnectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL)
+        await mongoose.connect('mongodb+srv://heroappman:63ko69NRhaRmQVdN@cluster-mi-banana.qtlcdaf.mongodb.net/Mi-Banana-database?retryWrites=true&w=majority')
     } catch (error) {
        console.log(error) 
     }
